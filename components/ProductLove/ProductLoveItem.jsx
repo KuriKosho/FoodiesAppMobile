a9d0550205df6d9cf59c77158054e3778c301501
chichiphan21 <tomunbin@gmail.com>
2024-04-12 07:59:50 +0700

    lovescreen

---
+++ components/ProductLove/ProductLoveItem.jsx
@@ -0,0 +1,87 @@
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
+import { FontAwesome, FontAwesome5, Ionicons } from '@expo/vector-icons';
+
+import React from 'react'
+
+const ProductLoveItem = ({ title, vote, time, level, image }) => {
+    return (
+        <TouchableOpacity style={styles.container}>
+            <View>
+                <Image source={image} style={styles.imgStyles} />
+            </View>
+            <View style={styles.iconContainer} >
+                <Text>{vote}</Text>
+                <FontAwesome style={styles.icon} name="star" size={14} color="orange" ></FontAwesome>
+            </View>
+            <View style={{ padding: 10 }}>
+
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title} numberOfLines={1}> {title}</Text>
+                </View>
+
+                <View style={styles.rateContainer}>
+                    <View style={styles.time}>
+                        <Ionicons name="time-outline" size={18} color="black" />
+                        <Text>{time}</Text>
+                    </View>
+                    <View>
+                        <Text>{level}</Text>
+                    </View>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+export default ProductLoveItem
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 25,
+        flex: 1,
+        marginVertical: 10,
+        borderColor: "black",
+        width: "100%",
+        overflow: 'hidden',
+        backgroundColor: '#E4E9F2',
+        position: "relative"
+    },
+    imgStyles: {
+        padding: 10,
+        width: '100%',
+        height: 180,
+        resizeMode: 'cover',
+    },
+    titleContainer: {
+        paddingTop: 10
+    },
+    title: {
+        fontSize: 15,
+        fontWeight: '500',
+
+    },
+    iconContainer: {
+        flexDirection: 'row',
+        position: "absolute",
+        top: 10,
+        left: 10,
+        backgroundColor: "white",
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 100
+    },
+    icon: {
+        padding: 3,
+    },
+    rateContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 10,
+        paddingTop: 10
+    },
+    time: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: "center"
+    }
+})
--- screens/MainScreen/FavoriteScreen.js
+++ screens/MainScreen/FavoriteScreen.js
@@ -1,24 +1,34 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Layout from "../../layouts/body/Layout"
+import SearchTool from '@/components/UI/SearchTool'
+import ListLoveItems from '@/components/ProductLove/ListLoveItems'
+import Category from '@/components/UI/Category'
 const FavoriteScreen = () => {
   return (
     <Layout>
-      <View style= {styles.container}>
-      <Text>FavoriteScreen</Text>
-    </View>  
+      <View style={styles.container}>
+        <Category title={"Favorite recipes"} style={styles.title} />
+        <SearchTool />
+        <ListLoveItems />
+      </View>
     </Layout>
-    
+
   )
 }
 
 export default FavoriteScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '##fff',
+    marginHorizontal: 15,
+    marginBottom: 50
+  },
+  title: {
+    color: '#cd6163',
+    fontWeight: '800',
+    fontSize: 25
+  }
 })
